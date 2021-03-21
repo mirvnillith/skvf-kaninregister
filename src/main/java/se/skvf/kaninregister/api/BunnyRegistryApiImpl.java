6169bf6cd81e0e1b4017fa91362fb3186cab86d8
Jonas Olsson <jonas201973@gmail.com>
2021-03-21 08:08:22 +0100

    removed unused import

--- src/main/java/se/skvf/kaninregister/api/BunnyRegistryApiImpl.java
+++ src/main/java/se/skvf/kaninregister/api/BunnyRegistryApiImpl.java
@@ -41,7 +41,6 @@ import java.util.stream.Collectors;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.ws.rs.Path;
 import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.ext.Provider;
