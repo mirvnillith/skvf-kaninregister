6e87329a4ba7f0735a7b110e4d8eb5989ac21ced
Kristian <kristian.alvarez@consecio.com>
2021-06-09 22:55:40 +0200

    Call api from register, handle errors

--- src/main/js/App.js
+++ src/main/js/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
 import Login from './Login'
 import Register from './Register'
@@ -21,12 +21,28 @@ const Content = (props) => {
         return <Login setView={props.setView}/>
     }
     if (props.view === "register") {
-        return <Register setView={props.setView}/>
+        return <Register setView={props.setView} setErrorMsg={props.setErrorMsg}/>
     }
 }
 
+const Error = (props) => {
+   if (props.errorMsg) {
+       return (
+            <div className="alert alert-danger alert-dismissible fade show" role="alert">
+                {props.errorMsg}
+                <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+            </div>
+       );
+   }
+   else {
+       return (null)
+   }
+}
+
+
 const App = () => {
     const [view, setView] = useState("login");
+    const [errorMsg, setErrorMsg] = useState("");
 
     return (
         <div className="container-md px-0">
@@ -36,8 +52,9 @@ const App = () => {
                     <h1 className="text-center green"> Kaninregister </h1>
                </div>
             </div>
+            <Error errorMsg={errorMsg}/>
             <div className="container">
-                <Content view={view} setView={setView}/>
+                <Content view={view} setView={setView} setErrorMsg={setErrorMsg}/>
             </div>
         </div>
     );
