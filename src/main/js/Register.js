a2ca3cd55850432dd7a58a04fce8df83158c3918
Kristian <kristian.alvarez@consecio.com>
2021-06-15 16:44:22 +0200

    Enable a logged-in state

--- src/main/js/Register.js
+++ src/main/js/Register.js
@@ -1,114 +1,26 @@
 import React, { useState } from 'react';
+import RegisterForm from './common/RegisterForm';
+import { createOwner, updateOwner, loginUser } from './utils/api';
+
 
 const Register = (props) => {
-    const [user, setUser] = useState("");
-    const [pwd, setPwd] = useState("");
-    const [pwd2, setPwd2] = useState("");
-    const [isValidPwd, setIsValidPwd] = useState(true);
-    const [isValidated, setIsValidated] = useState(false);
     const setError = (msg) => props.setNotification({type: "danger", msg: msg});
-
-    const submitHandler = async (e) => {
-        e.preventDefault();
-        setIsValidated(true);
-        if (pwd !== pwd2) {
-           setIsValidPwd(false);
+    const createRegistrationSuccessHandler = (userName, pwd) => {
+        const success = (user) => {
+            props.setSession(user)
         }
-        if(user && pwd && pwd2 && isValidPwd) {
-            const response = await fetch("/api/owners", {
-                method: 'POST',
-                headers: new Headers({'content-type': 'application/json'}),
-                body: JSON.stringify({userName: user, password: pwd})
-            });
-            const responseMsg = await response.json();
-            if (response.status === 200){
-                console.log(responseMsg);
-            }
-            else if (response.status === 400) {
-                console.error(responseMsg);
-                setError("Felaktigt användarnamn eller användare!")
-            }
-            else if (response.status === 409) {
-                console.error(responseMsg);
-                setError("Användarnamnet finns redan!")
-            }
-            else {
-                console.error(responseMsg);
-                setError("Ops, något gick fel!")
-            }
+        return async (_) => {
+            await loginUser(userName, pwd, success, setError)
         }
     }
+    const loginHandler = () => props.setView("login");
 
-    const loginHandler = (e) => {
-        e.preventDefault();
-        props.setView("login");
+    const submitForm = async (user, pwd) => {
+        await createOwner(user, pwd, createRegistrationSuccessHandler(user, pwd), setError)
     }
 
     return (
-        <div className="row py-2">
-            <form onSubmit={submitHandler} >
-                <div className="col-md-12">
-                    <h2 >Registrera dig</h2>
-                </div>
-                <div className="col-md-12">
-                    <div className="row mb-2">
-                        <label htmlFor="userName" className="col-md-6 col-form-label">Användarnamn</label>
-                        <div className="col-md-6">
-                            <input
-                                type="text"
-                                className="form-control"
-                                className={isValidated && user === "" ? "form-control is-invalid" : "form-control"}
-                                id="userName"
-                                onChange={e => setUser(e.target.value)}
-                            />
-                            <div className="invalid-feedback">
-                                Du måste ange ett användarnamn!
-                            </div>
-                        </div>
-                    </div>
-                    <div className="row mb-2">
-                        <label htmlFor="password" className="col-md-6 col-form-label">Lösenord</label>
-                        <div className="col-md-6">
-                            <input
-                                type="password"
-                                className={isValidated && pwd === "" ? "form-control is-invalid" : "form-control"}
-                                id="password"
-                                onChange={e => setPwd(e.target.value)}
-                            />
-                            <div className="invalid-feedback">
-                                Du måste ange ett lösenord!
-                            </div>
-                        </div>
-                    </div>
-                    <div className="row mb-2">
-                        <label htmlFor="password2" className="col-md-6 col-form-label">Upprepa Lösenord</label>
-                        <div className="col-md-6">
-                            <input
-                                type="password"
-                                className={isValidPwd ? "form-control" : "form-control is-invalid"}
-                                id="password2"
-                                onChange={e => setPwd2(e.target.value)}
-                            />
-                            <div className="invalid-feedback">
-                                Du måste upprepa ditt lösenord!
-                            </div>
-                        </div>
-                    </div>
-                    <div className="row mb-2">
-                        <div className="col-sm-8 align-self-end">
-                            <p className="mb-0">
-                                Har du redan ett konto?
-                                &nbsp;
-                                <a className="link-primary" onClick={loginHandler}>Logga in här!</a>
-                            </p>
-                        </div>
-                        <div className="col-sm-4">
-                            <button type="submit" className="btn btn-primary float-end" >Registrera dig</button>
-                        </div>
-                    </div>
-                </div>
-            </form>
-        </div>
+        <RegisterForm submitForm={submitForm} loginHandler={loginHandler} />
     );
 }
 
--- src/main/js/common/Notification.js
+++ src/main/js/common/Notification.js
@@ -2,10 +2,9 @@ import React from 'react';
 
 const Notification = (props) => {
     const type = props.type;
-    const key = props.key;
     const msg = props.msg
     return (
-        <div key={key} className={`alert alert-${type} alert-dismissible fade show`} role="alert">
+        <div className={`alert alert-${type} alert-dismissible fade show`} role="alert">
             {props.msg}
             <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
         </div>
