b98243f983ad93352d806297396c4c07224ab4b8
Jonas Olsson <jonas201973@gmail.com>
2022-08-12 08:26:00 +0200

    spelling

--- src/main/js/help/HelpPublicPrivate.js
+++ src/main/js/help/HelpPublicPrivate.js
@@ -7,7 +7,7 @@ const HelpPicture = (_) => {
 			<h2 className="text-center dark"> Publik/Privat </h2>
 		
 			Som publik ägare/uppfödare är dina uppgifter tillgängliga för de som ser en kanin du har relation till.
-			Det kan vara en kanin du fött upp, en där du står som nuvarande/föregående ägare eller en kanin något hittat och försöker återbörda.
+			Det kan vara en kanin du fött upp, en där du står som nuvarande/föregående ägare eller en kanin någon hittat och försöker återbörda.
 			<p/>
 			Som privat ägare/uppfödare är dina uppgifter endast tillgängliga för Kaninregistret.
 			Vi lämnar aldrig ut dessa till privatpersoner, men förmedlar meddelanden vid behov (exempelvis om någon hittat din kanin).
